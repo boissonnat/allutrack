ee927a51a302bac6c0644df084869c4e2be709d1
boissonnat <alexis.boissonnat@gmail.com>
2013-10-07 11:43:22 +0200

    UI for project.show, edit/remove labels in project

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -2,7 +2,7 @@ class LabelsController < ApplicationController
   before_filter :authenticate_user!
   load_and_authorize_resource
 
-  # Create
+  ## Create
   def new
     if params[:project_id]
       @label.project = Project.find(params[:project_id])
@@ -15,6 +15,27 @@ class LabelsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  ## Edit
+  def edit
+  end
+  def update
+    @project = @label.project
+    if @label.update_attributes(params[:label])
+      flash[:notice] = 'Successfully updated label.'
+      redirect_to @project
+    else
+      render 'edit'
+    end
+  end
+
+  # Delete
+  def destroy
+    @project = @label.project
+    @label.destroy
+    flash[:notice] = 'Successfully destroyed label.'
+    redirect_to @project
   end
 
   ## Helper methods
