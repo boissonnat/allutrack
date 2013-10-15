61e81134fab56ae23e77b848e02fa9f0aa041251
boissonnat <alexis.boissonnat@gmail.com>
2013-10-15 10:39:09 +0200

    Improve markdown cheat sheet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,9 @@ require 'redcarpet'
 module ApplicationHelper
     def markdown(text)
       renderer = Redcarpet::Render::HTML.new(render_options = {})
-      markdown = Redcarpet::Markdown.new(renderer, extensions = {})
+      markdown = Redcarpet::Markdown.new(renderer, extensions = {
+          :strikethrough => true
+      })
       markdown.render(text).html_safe
     end
 end
